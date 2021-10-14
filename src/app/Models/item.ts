144166fde551cf6266c480f53fffe1ae08159752
Nikita Manko <n.manko@itransition.com>
2021-10-14 11:41:19 +0300

    Collection

---
+++ src/app/Models/item.ts
@@ -0,0 +1,26 @@
+export class Item {
+  idItem:number=0;
+  idCollection:number=2;
+  nameItem:string="";
+  firstField_String:string="";
+  secondField_String:string="";
+  thirdField_String:string="";
+  firstField_Int:number=0;
+  secondField_Int:number=0;
+  thirdField_Int:number=0;
+
+  firstField_Data:string="";
+  secondField_Data:string="";
+  thirdField_Data:string="";
+
+  firstField_Bool:boolean=false;
+  secondField_Bool:boolean=false;
+  thirdField_Bool:boolean=false;
+
+  likes:string ="";
+  tags:string ="";
+
+  likesCount:number = 0;
+  commentsCount:number = 0;
+}
+
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Collection } from 'src/app/Models/Collection';
+import {CollectionsService} from "../../service/Collections.service";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  collections:Collection[]=[];
+  columns = ['id','author','title','price']
+  constructor(private  bs:CollectionsService) { }
 
   ngOnInit(): void {
+    this.bs.getAllCollections().subscribe(res=>{
+      this.collections=res
+    })
   }
 
+  AddCollection(Topic:string,CollectionName:string,Description:string){
+    this.bs.addCollections(Topic,CollectionName,Description).subscribe(res=>{
+      alert("Collection was added")
+    },
+      error => {
+      alert("Collection wasn't added")
+      })
+  }
 }
